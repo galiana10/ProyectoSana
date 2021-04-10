898187f21ce73c05b0c9de8954a7600096a8addf
dani10mv <danielmirallesvargas@gmail.com>
2021-04-10 11:51:38 +0200

    Dao de servicio con metodos

---
+++ sana/src/main/java/es/uji/ei1027/sana/dao/ServiceDAO.java
@@ -0,0 +1,75 @@
+package es.uji.ei1027.sana.dao;
+
+
+
+import es.uji.ei1027.sana.model.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.sql.DataSource;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class ServiceDAO {
+
+    private JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public void setDataSource(DataSource dataSource) {
+        jdbcTemplate = new JdbcTemplate(dataSource);
+    }
+
+
+    /* Afegeix el service a la base de dades */
+    public void addService(Service service) {
+        jdbcTemplate.update("INSERT INTO service VALUES(?, ?, ?)",
+              service.getName(),service.getDescription(),service.getServicetype()) ;
+    }
+
+    /*Esborra un servici*/
+    public void deleteService(Service service) {
+        jdbcTemplate.update("DELETE FROM service where name = ?",
+                service.getName()) ;
+    }
+
+    /*Esborra un servici*/
+    public void deleteService(String serviceName) {
+        jdbcTemplate.update("DELETE FROM service where name = ?",
+                serviceName) ;
+    }
+
+    /*Actualiza un service*/
+    public void updateService(Service service){
+        jdbcTemplate.update("UPDATE service set description=?,typeservice=?",
+                service.getDescription(),service.getServicetype());
+    }
+
+
+    /* Obté la classificacio amb el nom donat. Torna null si no existeix. */
+    public Service getService(String serviceName) {
+        try {
+            return jdbcTemplate.queryForObject("SELECT * from service WHERE name=?",
+                    new ServiceRowMapper(),serviceName);
+        }
+        catch(EmptyResultDataAccessException e) {
+            return null;
+        }
+    }
+
+    /* Obté la classificacio amb el nom donat. Torna null si no existeix. */
+    public List<Service> getServices() {
+        try {
+            return jdbcTemplate.query("SELECT * from service ",
+                    new ServiceRowMapper());
+        }
+        catch(EmptyResultDataAccessException e) {
+            return new ArrayList<Service>();
+        }
+    }
+
+
+
+
+}
